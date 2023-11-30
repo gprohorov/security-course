cb76d08612b0db3104276ccf7c172fe1cc423a82
gprohorov <g.prokhorov@indevlab.com>
2023-11-30 13:45:21 +0200

    cors

--- src/main/java/edu/pro/securitycourse/auth/AuthenticationService.java
+++ src/main/java/edu/pro/securitycourse/auth/AuthenticationService.java
@@ -115,5 +115,4 @@ public class AuthenticationService {
                 .tfaEnabled(user.isTfaEnabled())
                 .build();
     }
-
 }
--- src/main/java/edu/pro/securitycourse/config/SecurityConfig.java
+++ src/main/java/edu/pro/securitycourse/config/SecurityConfig.java
@@ -61,5 +61,4 @@ public class SecurityConfig {
                         );
         return http.build();
     }
-
 }
